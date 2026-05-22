13cd7f3550c9654ee86feb43f2bc8c058e7b86d1
fornamin <kmj1412@naver.com>
2026-05-22 16:28:10 +0900

    add: OpenAI API 실습

---
+++ 9. OpenAI/1. intro/1. restapi.py
@@ -0,0 +1,27 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+openai_api_key = os.getenv('OPENAI_API_KEY')
+user_input = '잘 먹었습니당'
+
+res = requests.post(
+    'https://api.openai.com/v1/chat/completions',
+    json={
+        'model': 'gpt-3.5-turbo',
+        'messages': [
+            {'role': 'system',
+             'content': '너는 한국의 배달 어플 사장이고 고객 리뷰에 대한 답글을 작성하면 돼'},
+            {'role': 'user',
+             'content': user_input}
+        ]
+    },
+    headers={
+        'Content-Type':'application/json',
+        'Authorization': f'Bearer {openai_api_key}'
+    }
+)
+
+data = res.json()
+print(data)
