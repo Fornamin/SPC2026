d7938e66fd693605625b9efacf9178730d34cd04
fornamin <kmj1412@naver.com>
2026-05-26 13:42:35 +0900

    update: 설명 보충

--- 9. OpenAI/1. intro/1. restapi.py
+++ 9. OpenAI/1. intro/1. restapi.py
@@ -7,9 +7,11 @@ openai_api_key = os.getenv('OPENAI_API_KEY')
 user_input = '잘 먹었습니당'
 
 res = requests.post(
+    # /v1/chat/completions: 문장을 완성
+    # /v1/responses: 응답 -> 서버 안에 메모리를 사용
     'https://api.openai.com/v1/chat/completions',
     json={
-        'model': 'gpt-3.5-turbo',
+        'model': 'gpt-3.5-turbo', #gpt-4, gpt-4o, gpt-4o-mini, gpt-5 등 다양한 모델
         'messages': [
             {'role': 'system',
              'content': '너는 한국의 배달 어플 사장이고 고객 리뷰에 대한 답글을 작성하면 돼'},
@@ -24,4 +26,4 @@ res = requests.post(
 )
 
 data = res.json()
-print(data)
+print(data['choices'][0]['message']['content'])
